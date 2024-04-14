21ddf1d3e001a7963de95515554788fae725d83f
Quentin Brooks <sliftist@gmail.com>
2024-04-14 15:39:15 -0400

    Update colors.

--- src/formatting/logColors.ts
+++ src/formatting/logColors.ts
@@ -5,14 +5,16 @@ function ansiHSL(h: number, s: number, l: number, text: string): string {
     return ansiRGB(r, g, b, text);
 }
 function ansiRGB(r: number, g: number, b: number, text: string): string {
-    return `\x1b[38;2;${r};${g};${b}m${text}\x1b[39m`;
+    return `\x1b[38;2;${r};${g};${b}m${text}\x1b[0m`;
 }
 
 const lightness = 68;
-export const blue = ansiHSL.bind(null, 235, 100, lightness);
+export const blue = (text: string) => `\x1b[34m${text}\x1b[0m`;
 export const red = ansiHSL.bind(null, 0, 100, lightness);
-export const green = (text: string) => `\x1b[32m${text}\x1b[39m`; // chalk.hsl(120, 100, lightness).bind(chalk);
-export const yellow = (text: string) => `\x1b[33m${text}\x1b[39m`;
+export const green = (text: string) => `\x1b[32m${text}\x1b[0m`;
+export const yellow = (text: string) => `\x1b[33m${text}\x1b[0m`;
 export const white = ansiHSL.bind(null, 0, 0, 80);
 
-export const magenta = (text: string) => `\x1b[35m${text}\x1b[39m`;
+export const magenta = (text: string) => `\x1b[35m${text}\x1b[0m`;
+
+console.log(blue('blue'));
